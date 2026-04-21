4aec184656be00e86b888081a3227fa709774da2
Sher <harshkarira2004@gmail.com>
2026-04-21 22:21:09 +0530

    added the https statement

--- src/main/java/com/Harsh/jpaTutorial/config/SecurityConfig.java
+++ src/main/java/com/Harsh/jpaTutorial/config/SecurityConfig.java
@@ -78,7 +78,7 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of(
                 "http://localhost:5173",
-                "job-trackerweb.netlify.app"
+                "https://job-trackerweb.netlify.app"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
